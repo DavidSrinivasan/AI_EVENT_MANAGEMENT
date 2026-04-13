f8dd06296e38c84f40d1456c9ae25094a584759a
DavidSrinivasan <davidsrinivasan11@gmail.com>
2026-04-13 14:51:18 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import EventDetailsPage from './pages/EventDetailsPage';
-import UserProfilePage from './pages/UserProfilePage';
+import VenueSearchPage from './pages/VenueSearchPage';
+import { VenueDetailPage, CreateEventPage, DashboardPage, ProfilePage } from './pages/AllPages';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/event/:id" element={<EventDetailsPage />} />
-        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="/venues" element={<VenueSearchPage />} />
+        <Route path="/venue/:id" element={<VenueDetailPage />} />
+        <Route path="/create-event" element={<CreateEventPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
